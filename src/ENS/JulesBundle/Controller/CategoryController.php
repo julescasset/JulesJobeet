ada7da6f7f1873e29df445765ccc3f8454fd2c01
julescasset <casset.jules@gmail.com>
2016-05-27 16:19:44 +0200

    add category page

--- src/ENS/JulesBundle/Controller/CategoryController.php
+++ src/ENS/JulesBundle/Controller/CategoryController.php
@@ -16,5 +16,21 @@ use Ens\JulesBundle\Entity\Category;
  */
 class CategoryController extends Controller
 {
+    public function showAction($slug)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $category = $em->getRepository('EnsJulesBundle:Category')->findOneBySlug($slug);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Unable to find Category entity.');
+        }
+
+        $category->setActiveJobs($em->getRepository('EnsJulesBundle:Job')->getActiveJobs($category->getId()));
+
+        return $this->render('EnsJulesBundle:Category:show.html.twig', array(
+            'category' => $category,
+        ));
+    }
 
 }
