8d68829c92cf0a7e8a7a2562f8a2b8b1c2af1229
julescasset <casset.jules@gmail.com>
2016-05-27 16:05:09 +0200

    add category controller and database update

---
+++ src/ENS/JulesBundle/Controller/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jules
+ * Date: 27/05/2016
+ * Time: 15:44
+ */
+
+namespace Ens\JulesBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ens\JulesBundle\Entity\Category;
+
+/**
+ * Category controller.
+ *
+ */
+class CategoryController extends Controller
+{
+
+}
