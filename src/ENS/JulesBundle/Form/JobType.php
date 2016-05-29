221eadae36e332ab4862ee9ddee6d2bd54b6f465
julescasset <casset.jules@gmail.com>
2016-05-29 16:27:03 +0200

    Job offer form personalisation

--- src/ENS/JulesBundle/Form/JobType.php
+++ src/ENS/JulesBundle/Form/JobType.php
@@ -4,23 +4,25 @@ namespace Ens\JobeetBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Ens\JulesBundle\Entity\Job;
 
 class JobType extends AbstractType
 {
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder->add('category');
-        $builder->add('type');
+        $builder->add('type', 'choice', array('choices' => Job::getTypes(), 'expanded' => true));
         $builder->add('company');
-        $builder->add('logo');
+        $builder->add('logo', null, array('label' => 'Company logo'));
         $builder->add('url');
         $builder->add('position');
         $builder->add('location');
         $builder->add('description');
-        $builder->add('how_to_apply');
+        $builder->add('how_to_apply', null, array('label' => 'How to apply?'));
         $builder->add('token');
-        $builder->add('is_public');
+        $builder->add('is_public', null, array('label' => 'Public?'));
         $builder->add('email');
+
     }
 
     public function getName()
